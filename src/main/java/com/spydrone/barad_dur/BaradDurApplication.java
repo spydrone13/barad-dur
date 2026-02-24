d30415608960676ba76963cc1769b57de03e597c
Spydrone <morri909@gmail.com>
2026-02-23 18:18:08 -0600

    Spring boot time

---
+++ src/main/java/com/spydrone/barad_dur/BaradDurApplication.java
@@ -0,0 +1,13 @@
+package com.spydrone.barad_dur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaradDurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaradDurApplication.class, args);
+	}
+
+}
