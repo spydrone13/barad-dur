2d895a02c3ca0c6497c871f7b222ca87a268c3af
Spydrone <morri909@gmail.com>
2026-02-27 19:22:51 -0600

    Create task dir

--- src/main/java/com/spydrone/baraddur/tasks/LotStageEventProcessor.java
+++ src/main/java/com/spydrone/baraddur/tasks/LotStageEventProcessor.java
@@ -1,7 +1,8 @@
-package com.spydrone.baraddur.service;
+package com.spydrone.baraddur.tasks;
 
 import com.spydrone.baraddur.repository.LotRepository;
 import com.spydrone.baraddur.repository.StageEventRepository;
+import com.spydrone.baraddur.service.WeeklyProjectionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
